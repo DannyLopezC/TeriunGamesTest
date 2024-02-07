d649c3c9c4a35568f66aa155044dde5a06f328eb
Danny López Cárdenas <danny.lopezc00@gmail.com>
2024-02-07 01:14:34 -0500

    Movement Reworked

--- Assets/Project/Scripts/Player/Player.cs
+++ Assets/Project/Scripts/Player/Player.cs
@@ -3,16 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Fusion;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : NetworkBehaviour {
-    [SerializeField] private float playerHeight;
-    private bool grounded;
-    [SerializeField] float groundDrag;
-    [SerializeField] private LayerMask whatIsGround;
+    private NetworkCharacterControllerPrototype _networkCharacterControllerPrototype;
 
-    [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform orientation;
     [SerializeField] private Transform body;
 
@@ -22,20 +17,13 @@ public class Player : NetworkBehaviour {
 
     [SerializeField] private TMP_Text _playerNameText;
 
+    private void Awake() {
+        _networkCharacterControllerPrototype = GetComponent<NetworkCharacterControllerPrototype>();
+    }
+
     private void Start() {
         GameManager.Instance.SetPlayerName += SetName;
         GameManager.Instance.SetCameraProperties.Invoke(orientation, transform, body);
-    }
-
-    private void Update() {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-
-        if (grounded) {
-            rb.drag = groundDrag;
-        }
-        else {
-            rb.drag = 0;
-        }
     }
 
     public override void FixedUpdateNetwork() {
@@ -43,8 +31,7 @@ public class Player : NetworkBehaviour {
             Vector3 moveDirection =
                 orientation.forward * data.MovementInput.y + orientation.right * data.MovementInput.x;
 
-            rb.AddForce(moveDirection.normalized * (speed * 10f),
-                ForceMode.Force);
+            _networkCharacterControllerPrototype.Move(moveDirection.normalized * speed * 10f);
         }
     }
 
